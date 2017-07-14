ac0f050deb6ca5e5e7fc4dedd4d538e97b6df025
MFMolaGit <manuelf.mola@gmail.com>
2017-07-14 21:22:00 +0200

    Primer commit

---
+++ patient-dispatcher-services/src/main/java/com/gevapps/patientdispatcherservices/PatientDispatcherApplication.java
@@ -0,0 +1,12 @@
+package com.gevapps.patientdispatcherservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientDispatcherApplication.class, args);
+	}
+}
---
+++ patient-dispatcher-services/src/test/java/com/gevapps/patientdispatcherservices/PatientDispatcherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gevapps.patientdispatcherservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatientDispatcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
